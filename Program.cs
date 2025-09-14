a7d11d6a731cdd99852cbb4d015d0c0e259038d8
Ricardo Rios <141197543+ricks-xz@users.noreply.github.com>
2025-09-14 08:48:57 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,84 @@
-﻿namespace CalculadoraSimples
-{
+using System;
+
     public class Program
     {
-        public static void Main()
+    public static void Main()
+    {
+
+        while (true)
         {
 
-            Console.WriteLine("<------- Calculadora Super Simples ------->");
+            Console.WriteLine("------ Calculadora Simples -------\n");
 
             Console.WriteLine("Digite o primeiro número: ");
-            int number1 = Convert.ToInt32(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Digite o segundo número: ");
-            int number2 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-            int soma = number1 + number2;
-            int subtracao = number1 - number2;
-            int multiplicacao = number1 * number2;
-            int divisao = number1 / number2;
-            int resto = number1 % number2;
+            Console.WriteLine("Escolha a operação: ");
+            Console.WriteLine("1 - Adição (+)");
+            Console.WriteLine("2 - Subtração (-)");
+            Console.WriteLine("3 - Multiplicação (*)");
+            Console.WriteLine("4 - Divisão (/)");
 
-            Console.WriteLine("\nResultados:");
-            Console.WriteLine($"O valor da soma é: {soma} \nO valor da subtracao é: {subtracao} \nO valor da multiplicacao é: {multiplicacao} \nO valor da divisao é: {divisao} \nO valor do resto é: {resto}");
+            Console.Write("Digite o número da operação desejada: ");
+            dynamic operacao = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("<--------------------------------------->");
+            double result = 0;
+
+            if (operacao == 1)
+            {
+                result = num1 + num2;
+                Console.WriteLine($"\n--> Resultado da soma: {result}");
+            }
+            else if (operacao == 2)
+            {
+                result = num1 - num2;
+                Console.WriteLine($"\n--> Resultado da subtração: {result}");
+            }
+            else if (operacao == 3)
+            {
+                result = num1 * num2;
+                Console.WriteLine($"\n--> Resultado da multiplicação: {result}");
+            }
+            else if (operacao == 4)
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"\n--> Resultado da divisão: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("\nX Erro: Divisão por zero não é permitida. X");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("\nX-- Operaçao inválida! --X");
+            }
+
+            Console.WriteLine("\nDeseja continuar? (s/n)");
+            string continuar = Console.ReadLine();
+
+            if (continuar?.ToLower() == "n")
+            {
+                Console.WriteLine("\nEncerrando a calculadora. Muito obrigado!");
+                break;
+            }
+            else if (continuar?.ToLower() == "s")
+            {
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("\nX-- Entrada inválida! Encerrando a calculadora. --X");
+                break;
+            }
+
         }
+
     }
 }
